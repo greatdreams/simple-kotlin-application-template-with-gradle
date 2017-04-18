40502726f1b5f59969d7f31a258408780ab941c9
Wang Huawei <871155310@qq.com>
2017-04-18 09:53:58 +0800

    fix test buf

--- src/test/kotlin/com/greatdreams/kotlin/template/tests.kt
+++ src/test/kotlin/com/greatdreams/kotlin/template/tests.kt
@@ -5,6 +5,7 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.junit.runner.RunWith
 
 object SimpleSpec: Spek({
             describe("a calculator") {
