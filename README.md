f977514ce412b80a125be20febcf132e44776e98
greatdreams <871155310@qq.com>
2017-10-19 19:39:04 +0800

    new add spek test classes

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # simple-kotlin-application-template-with-gradle
 This is a kotlin application template with gradle build tool.
 
+#### upgrade gradle wrapper version
+
+```bash
+$ ./gradlew wrapper --gradle-version=4.2.1
+```
+
 # Reference
 1. [Spek Framework](http://spekframework.org/)  A Kotlin Specification Framework for the JVM
 2. [hamrest](https://github.com/npryce/hamkrest) A reimplementation of Hamcrest to take advantage of Kotlin language features.
--- src/test/kotlin/com/greatdreams/kotlin/template/tests.kt
+++ src/test/kotlin/com/greatdreams/kotlin/template/tests.kt
@@ -1,13 +1,12 @@
 package com.greatdreams.kotlin.template
 
 import com.winterbe.expekt.should
+import junit.framework.Assert.assertEquals
 import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.*
 
 object SimpleSpec: Spek({
-            describe("a calculator") {
+            xdescribe("a calculator") {
                 on("addition" ) {
                     val sum = 100 + 200
                     it("should work") {
@@ -24,3 +23,55 @@ object SimpleSpec: Spek({
             }
         }
 )
+
+object CalculatorSpec: Spek({
+    given("a calculator") {
+        val num = 100
+        val num1 = 200
+        on("addition") {
+            val sum = num + num1
+            it("should return the result of adding the first number to the second number") {
+                assertEquals(300, sum)
+            }
+        }
+        on("subtraction") {
+            val result = num - num1
+            xit("should return the result of substracting the second number from the first") {
+                assertEquals(-100, result)
+            }
+        }
+    }
+})
+
+object FixtureSpec: Spek({
+    describe("a group") {
+        beforeGroup {
+
+        }
+        beforeEachTest {
+
+        }
+
+        context("a nest group") {
+            beforeEachTest {
+
+            }
+            beforeEachTest {
+
+            }
+            it("should work") {
+
+            }
+            afterEachTest {
+
+            }
+        }
+        afterEachTest {
+
+        }
+
+        afterGroup {
+
+        }
+    }
+})
