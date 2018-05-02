c82f58d486a0dd90f804fa4449d730319de29025
greatdreams <871155310@qq.com>
2018-05-03 00:36:14 +0800

    upgrade kotlin to 1.2.41

--- learn-scala/src/main/scala/com.greatdreams.learn.scala/MainProgram.scala
+++ learn-scala/src/main/scala/com.greatdreams.learn.scala/MainProgram.scala
@@ -8,7 +8,6 @@ object MainProgram {
   val logger: Logger = LoggerFactory.getLogger(MainProgram.getClass)
   def main(args: Array[String]):Unit = {
     logger.info("Hello, scala")
-
     ImplicitTest.main(args)
     ImplicitClassTest.main(args)
     CurrencyDSLTest.main(args)
